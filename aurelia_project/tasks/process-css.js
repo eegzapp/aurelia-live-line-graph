12bc061620c529f957c6f038e3f158c0cbdf62da
Mike Egan <mike.egan@sandisk.com>
2016-12-27 14:42:55 -0800

    Modified css processor to include from multiple sources

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -3,10 +3,25 @@ import sourcemaps from 'gulp-sourcemaps';
 import sass from 'gulp-sass';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
+import merge from 'merge2';
 
 export default function processCSS() {
+  /** original (only read single css source) 
   return gulp.src(project.cssProcessor.source)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(build.bundle());
+  */
+
+    var pipes = [];
+
+    project.cssProcessor.source.forEach(cssSource => {
+        pipes.push(
+            gulp.src(cssSource)
+              .pipe(sourcemaps.init())
+              .pipe(sass().on('error', sass.logError))
+              .pipe(build.bundle()));
+    });
+
+    return merge(...pipes);
 }
