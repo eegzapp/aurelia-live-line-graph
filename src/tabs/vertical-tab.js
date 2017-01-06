3b02f7472d88a5cd651ee174e5bef94b8e765fab
Mike Egan <mike.egan@sandisk.com>
2017-01-06 12:50:46 -0800

    - Passed image and text into vertical tabs - Changed container to 'container' (was 'container.fluid' previously) and set up padding - Updated colors and icons

--- src/tabs/vertical-tab.js
+++ src/tabs/vertical-tab.js
@@ -1,6 +1,12 @@
+import {bindable} from 'aurelia-framework';
 
 export class VerticalTab {
+    @bindable tabIcon;
+    @bindable tabText;
+
     constructor() {
-        
+        //console.log(this.icon);
+        //this.imageSrc = "fa fa-camera-retro fa-3x";
     }
+
 }
